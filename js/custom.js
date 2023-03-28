8a664808e0a580d7cd3d1c9e3ed24acdcb652086
Yreck-RU <74424990+Yreck-RU@users.noreply.github.com>
2023-03-29 02:52:58 +0300

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -363,3 +363,58 @@ window.onload = function() { // можно также использовать w
 		slaiderIndexActive.classList.add('_active');
 	}
 };
+
+
+
+
+
+
+
+// =================================================================
+// Начало - "Линивая загруска"
+// =================================================================
+
+const lazyImages = document.querySelectorAll('img[data-src], source[data-srcset]');
+
+const loadMapBlock = document.querySelector('._load-map');
+const windowHeight = document.documentElement.clientHeight;
+
+let lazyImagesPositions = [];
+if (lazyImages.length > 0) {
+	lazyImages.forEach(img => {
+		if (img.dataset.src || img.dataset.srcset) {
+			lazyImagesPositions.push(img.getBoundingClientRect().top + pageYOffset);
+			lazyScrollCheck();
+			ibg();
+		}
+	});
+}
+
+window.addEventListener("scroll", lazuScroll);
+function lazuScroll() {
+	if (document.querySelectorAll('img[data-src], source[data-srcset]').length > 0) {
+		lazyScrollCheck();
+		ibg();
+	}
+}
+
+function lazyScrollCheck() {
+	let imgIndex = lazyImagesPositions.findIndex(
+		item => pageYOffset > item - windowHeight
+	);
+
+	if (imgIndex >= 0) {
+		if (lazyImages[imgIndex].dataset.src) {
+			lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
+			lazyImages[imgIndex].removeAttribute('data-src');
+		} else if (lazyImages[imgIndex].dataset.srcset) {
+			lazyImages[imgIndex].srcset = lazyImages[imgIndex].dataset.srcset;
+			lazyImages[imgIndex].removeAttribute('data-srcset');
+		}
+		delete lazyImagesPositions[imgIndex];
+	}
+}
+
+// =================================================================
+// Конец - "Линивая загруска"
+// =================================================================
